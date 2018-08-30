b50fa6a2a0ecca88b3003f36ba9903319bc9fe5f
xieguoyong <xie.guoyong@dr-elephant.com>
2018-08-30 11:49:41 +0800

    333

--- doctor/test_case/myscheduling_sta.py
+++ doctor/test_case/myscheduling_sta.py
@@ -5,12 +5,28 @@ import time
 
 
 class MyScheduling(myunit.MyTest):
-    # 添加排期
     def test_add_scheduling_success(self):
+        """成功添加排期用例"""
         lo = myschedulingpage.MySchedulingPage(self.driver)
-        ss = lo.add_scheduling('18:30', '19:00')
+        ss = lo.add_scheduling('19:30', '20:00')
         self.assertTrue(ss)
         filename = '添加排期成功' + time.strftime("%Y%m%d%H%M%S", time.localtime()) + '.png'
+        screenshot.insert_img(self.driver, filename)
+
+    def test_add_scheduling_fail1(self):
+        """排期时间已存在用例"""
+        lo = myschedulingpage.MySchedulingPage(self.driver)
+        ss = lo.add_scheduling('19:30', '20:00')
+        self.assertFalse(ss)
+        filename = '排期时间已存在' + time.strftime("%Y%m%d%H%M%S", time.localtime()) + '.png'
+        screenshot.insert_img(self.driver, filename)
+
+    def test_add_scheduling_fail2(self):
+        """排期时间在当前时间之前用例"""
+        lo = myschedulingpage.MySchedulingPage(self.driver)
+        ss = lo.add_scheduling('7:30', '8:00')
+        self.assertTrue(ss)
+        filename = '排期时间在当前时间之前' + time.strftime("%Y%m%d%H%M%S", time.localtime()) + '.png'
         screenshot.insert_img(self.driver, filename)
 
 
